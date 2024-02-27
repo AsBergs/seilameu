c7c464eba90179d609abd66bb1b7c74c3ba2d695
AsBergs <anasofiabergsoares06@gmail.com>
2024-02-27 14:56:52 -0300

    teste2

--- src/components/ListagemProdutos.vue
+++ src/components/ListagemProdutos.vue
@@ -1,11 +1,12 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 import axios from 'axios';
-const produtos = ref([]);
+
+const ListagemProdutos = ref([]);
 
 onMounted(async () => {
   const response = await axios.get('https://fakestoreapi.com/products');
-  produtos.value = response.data;
+  ListagemProdutos.value = response.data;
 });
 
 
@@ -15,7 +16,7 @@ const formatPrice = (price) => `R$ ${price.toFixed(2).replace('.', ',')}`;
   <div>
     <h1>Produtos</h1>
     <div class="container">
-      <div class="card" v-for="produto in produtos" :key="produto.id">
+      <div class="card" v-for="produto in ListagemProdutos" :key="produto.id">
         <h1 class="card--title">{{ produto.title }}</h1>
         <p>{{ produto.description }}</p>
         <p>{{ formatPrice(produto.price) }}</p>
